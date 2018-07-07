a36d229dba08ab1630e38328ca30ac5428a59f78
Adam Wieckert <wiec1369@gmail.com>
2018-07-07 14:09:19 -0500

    Requests are working

--- src/App/App.js
+++ src/App/App.js
@@ -1,24 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {Route, BrowserRouter, Redirect, Switch} from 'react-router-dom';
+import firebase from 'firebase';
 import './App.css';
-// import {Fish} from '../Fish/Fish.js';
 import {Home} from '../Home/Home.js';
 import {Inventory} from '../Inventory/Inventory.js';
-// import {Login} from '../Login/Login.js';
+import {Login} from '../Login/Login.js';
 import {Navbar} from '../Navbar/Navbar.js';
-// import {New} from '../New/New.js';
-// import {Order} from '../Order/Order.js';
-// import {OrderSpa} from '../OrderSpa/OrderSpa.js';
-// import {Register} from '../Register/Register.js';
-// import {SingleOrder} from '../SingleOrder/SingleOrder.js';
+import {New} from '../New/New.js';
+import {OrderSpa} from '../OrderSpa/OrderSpa.js';
+import {Register} from '../Register/Register.js';
+import {SingleOrder} from '../SingleOrder/SingleOrder.js';
+import fbConnection from '../FirebaseRequests/connection.js';
+fbConnection();
 
-const PrivateRoute = ({ component: component, authed, ...rest}) => {
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
   return (
     <Route
       {...rest}
       render={props =>
         authed === true ? (
-          <component {...props} />
+          <Component {...props} />
         ) : (
           <Redirect
             to={{ pathname: '/login', state: {from: props.location}}}
@@ -29,22 +30,62 @@ const PrivateRoute = ({ component: component, authed, ...rest}) => {
   );
 };
 
-class App extends Component {
+const PublicRoute = ({component: Component, authed, ...rest}) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authed === false ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: '/orders', state: {from: props.location}}}
+          />
+        )
+      }
+    />
+  );
+};
+
+class App extends React.Component {
   state={
     authed: false,
   }
+
+  componentDidMount () {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({authed: true});
+      } else {
+        this.setState({authed: false});
+      }
+    });
+  }
+
+  componentWillUnmount () {
+    this.removeListener();
+  };
+
+  runAway = () => {
+    this.setState({authed: false});
+  };
 
   render () {
     return (
       <div>
         <BrowserRouter>
           <div>
-            <Navbar />
+            <Navbar authed={this.state.authed} runAway={this.runAway} />
             <div className='container'>
               <div className='row'>
                 <Switch>
                   <Route path='/' exact component={Home}/>
                   <PrivateRoute path='/inventory' authed={this.state.authed} component={Inventory}/>
+                  <PublicRoute path='/register' authed={this.state.authed} component={Register}/>
+                  <PublicRoute path='/login' authed={this.state.authed} component={Login}/>
+                  <PrivateRoute path='/orders' authed={this.state.authed} component={OrderSpa}/>
+                  <PrivateRoute path='/orders/:id' authed={this.state.authed} component={SingleOrder}/>
+                  <PrivateRoute path='/New' authed={this.state.authed} component={New}/>
                 </Switch>
               </div>
             </div>
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,11 +1,52 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import authRequests from '../FirebaseRequests/auth.js';
 import './Navbar.css';
 
 class Navbar extends Component {
   render () {
+    const {authed, runAway} = this.props;
+    const logoutClickEvent = () => {
+      authRequests.logoutUser();
+      runAway();
+    };
     return (
-      <div className='Navbar text-center'>
-        <h1>Navbar</h1>
+      <div className="Navbar">
+        <nav className="navbar navbar-inverse">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <Link to='/' className='navbar-brand'>Fish Store</Link>
+            </div>
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              { authed ? (
+                <ul className="nav navbar-nav navbar-right">
+                  <li>
+                    <Link to='/inventory'>Inventory</Link>
+                  </li>
+                  <li>
+                    <Link to='/orders'>Orders</Link>
+                  </li>
+                  <li className='navbar-form'>
+                    <button className='btn btn-danger' onClick={logoutClickEvent}>Log-Out</button>
+                  </li>
+                </ul>
+              ) : (
+                <ul className="nav navbar-nav navbar-right">
+                  <li>
+                    <Link to='/login'>Login</Link>
+                  </li>
+                </ul>
+              )
+              }
+            </div>
+          </div>
+        </nav>
       </div>
     );
   };
