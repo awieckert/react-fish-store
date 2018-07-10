6ef9002e2ad3af04df7e7af7052522cf4be79b5f
Adam Wieckert <wiec1369@gmail.com>
2018-07-09 21:01:42 -0500

    Have my orders displaying and the new order button routing

--- src/OrderSpa/OrderSpa.js
+++ src/OrderSpa/OrderSpa.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import moment from 'moment';
+import orderRequests from '../FirebaseRequests/orders.js';
+import authRequests from '../FirebaseRequests/auth.js';
 import './OrderSpa.css';
 
 class OrderSpa extends Component {
+  state = {
+    orders: [],
+  };
+
+  componentDidMount () {
+    orderRequests.getRequest(authRequests.getUid()).then((orders) => {
+      this.setState({orders});
+    }).catch((err) => {
+      console.error('Failed to grab fishes: ', err);
+    });
+  };
+
   render () {
+    const orderComponents = this.state.orders.map((order) => {
+      return (
+        <button key={order.id} className='col-xs-12 btn btn-default order-button'>{order.id}
+        <span className='col-xs-6'>Order Number: {order.id}</span>
+        <span className='col-xs-6'>Order Date: {moment(order.dateTime).format('LLL')}</span>
+        </button>
+      );
+    });
     return (
-      <div className='OrderSpa text-center'>
-        <h1>OrderSpa</h1>
+      <div className='OrderSpa col-xs-12'>
+        <h2>Orders</h2>
+        <button><Link to='/new'>New Order</Link></button>
+        <ul>
+          {orderComponents}
+        </ul>
       </div>
     );
   };
