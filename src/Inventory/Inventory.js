b5d86cb09bd95b29ac4b2df48f508e69cbfc942a
Adam Wieckert <wiec1369@gmail.com>
2018-07-09 19:25:51 -0500

    Get request working

--- src/Inventory/Inventory.js
+++ src/Inventory/Inventory.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import fishRequests from '../FirebaseRequests/fishes.js';
 import './Inventory.css';
 
 class Inventory extends Component {
+  state = {
+    fishes: [],
+  };
+
+  componentDidMount () {
+    fishRequests.getRequest().then((fishes) => {
+      this.setState({fishes});
+    }).catch((err) => {
+      console.error('Failed to grab fishes: ', err);
+    });
+  };
+
   render () {
+    const fishComponents = this.state.fishes.map((fish) => {
+      return (
+        <h2>{fish.name}</h2>
+      );
+    });
     return (
       <div className='Inventory text-center'>
         <h1>Inventory</h1>
+        <ul className='fishes'>
+          {fishComponents}
+        </ul>
       </div>
     );
   };
